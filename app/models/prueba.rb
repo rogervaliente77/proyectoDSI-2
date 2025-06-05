9ce44dc00db12e060d0bbfa57a489dcb4ce5ffe0
rogervaliente7 <rogervalriveraj@gmail.com>
2025-06-05 13:44:19 +0000

    changing to postgresql

--- app/models/prueba.rb
+++
@@ -1,6 +0,0 @@
-class Prueba
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  field :nombre, type: String
-  field :description, type: String
-end
--- config/application.rb
+++ config/application.rb
@@ -1,28 +1,26 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "active_storage/engine"
+require "rails/test_unit/railtie"
+
+require "mongoid/railtie"
+
 Bundler.require(*Rails.groups)
 
 module AuthWithRails
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = "America/El_Salvador"
   end
 end
