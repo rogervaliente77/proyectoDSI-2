990f20990ab3fd5b482183502e76ca1ffe0d5ab8
Marvin Eduardo Rodriguez Dominguez <rd18025@ues.edu.sv>
2025-09-07 14:35:17 -0600

    Carrusel hover-driven con flechas y tarjeta profesional

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,23 +1,22 @@
+# app/models/product.rb
 class Product
   include Mongoid::Document
-  include Mongoid::Timestamps  # añade created_at y updated_at
+  include Mongoid::Timestamps
 
-  # Campos
   field :name,        type: String
   field :description, type: String
   field :quantity,    type: Integer
   field :price,       type: Float
-  field :image_url,   type: String
   field :code,        type: String
 
-  # Relaciones
-  has_many   :product_sales, dependent: :destroy
+  has_many :product_sales, dependent: :destroy
   belongs_to :category
 
-  # Validaciones
+  embeds_many :product_images
+  accepts_nested_attributes_for :product_images, allow_destroy: true
+
   validates :name, :price, :quantity, presence: true
 
-  # Callbacks
   before_create :generate_code
 
   private
@@ -27,8 +26,6 @@ class Product
 
     prefix   = category.name[0, 3].upcase
     date_str = Date.today.strftime("%Y%m%d")
-
-    # Contar productos creados hoy en la misma categoría
     count_today = Product.where(
       :created_at.gte => Date.today.beginning_of_day,
       :created_at.lt  => Date.today.end_of_day,
@@ -36,8 +33,7 @@ class Product
     ).count
 
     sequence   = count_today + 1
-    padded_seq = sequence.to_s.rjust(3, '0') # "001", "002", etc.
-
+    padded_seq = sequence.to_s.rjust(3, '0')
     self.code = "#{prefix}-#{date_str}-#{padded_seq}"
   end
 end
---
+++ app/models/product_image.rb
@@ -0,0 +1,12 @@
+# app/models/product_image.rb
+class ProductImage
+  include Mongoid::Document
+
+  field :title,       type: String
+  field :image_url,   type: String
+  field :image_index, type: Integer
+
+  embedded_in :product
+
+  validates :image_url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "debe ser una URL válida" }
+end
