2cde0a574122fdbae26db067728c2ed927b10f8f
Marvin Eduardo Rodriguez Dominguez <rd18025@ues.edu.sv>
2025-10-16 22:53:53 -0600

    Corrigiendo Movimientos de inventario

--- app/controllers/admin/product_histories_controller.rb
+++ app/controllers/admin/product_histories_controller.rb
@@ -5,7 +5,26 @@ module Admin
     before_action :set_product_history, only: [:show]
 
     def index
-      @product_histories = ProductHistory.all.order_by(created_at: :desc)
+      @product_histories = ProductHistory.all
+
+      # 🔹 Filtro por rango de fechas
+      if params[:start_date].present? && params[:end_date].present?
+        start_date = DateTime.parse(params[:start_date]).beginning_of_day
+        end_date   = DateTime.parse(params[:end_date]).end_of_day
+        @product_histories = @product_histories.where(:created_at.gte => start_date, :created_at.lte => end_date)
+      end
+
+      # 🔹 Filtro por nombre parcial de usuario
+      if params[:user_name].present?
+        name_query = Regexp.new(Regexp.escape(params[:user_name]), Regexp::IGNORECASE)
+        # Obtener los IDs de usuarios que coincidan
+        matching_user_ids = User.or({ first_name: name_query }, { last_name: name_query }).pluck(:id)
+        @product_histories = @product_histories.where(:user_id.in => matching_user_ids)
+      end
+
+      # 🔹 Ordenar por fecha descendente
+      @product_histories = @product_histories.order_by(created_at: :desc)
+
       render "admin/products/histories_index"
     end
 
