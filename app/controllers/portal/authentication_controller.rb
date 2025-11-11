2653df45de6fcc945e60ad7ffaf82f5d0c816395
rogervaliente77 <vr20033@ues.edu.sv>
2025-11-10 19:39:20 -0600

    Resolviendo signup3

--- app/controllers/portal/authentication_controller.rb
+++ app/controllers/portal/authentication_controller.rb
@@ -94,7 +94,7 @@ module Portal
       if @user.save
         # Si el registro es exitoso
         session[:jwt_token] = @user.jwt_token
-        UserVerificationMailer.send_otp_email(@user).deliver_now
+        UserVerificationMailer.send_otp_email(@user).deliver_later
         redirect_to portal_validating_user_path, notice: "Solicitud recibida con exito"
       else
         # Si hay errores, renderiza el formulario nuevamente
