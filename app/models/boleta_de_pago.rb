75a97dcee34ec8d178eb5ad0c54203fd7492f974
rogervaliente77 <vr20033@ues.edu.sv>
2026-06-22 23:24:49 -0600

    Nuevos cambios para ver boleta

--- app/models/boleta_de_pago.rb
+++ app/models/boleta_de_pago.rb
@@ -1,26 +1,18 @@
 class BoletaDePago
   include Mongoid::Document
-  include Mongoid::Timestamps # Para created_at y updated_at automáticos
-  
-  belongs_to :empleado
+  include Mongoid::Timestamps
+
   belongs_to :planilla
+  belongs_to :empleado
 
-  # Snapshot de valores al momento de generar la planilla
-  field :sueldo_base_momento, type: Float
-  field :dias_trabajados, type: Integer
-  
-  # Ingresos
-  field :monto_horas_extra, type: Float
-  field :otros_ingresos, type: Float
-  
-  # Descuentos de Ley (SV)
-  field :isss_retencion, type: Float
-  field :afp_retencion, type: Float
-  field :renta_retencion, type: Float
-  
-  # Otros descuentos (Préstamos, faltas, etc.)
-  field :descuento_faltas, type: Float
-  field :prestamos_internos, type: Float
-
-  field :total_neto, type: Float # El "Líquido a recibir"
+  field :sueldo_base_momento, type: Float, default: 0.0
+  field :dias_trabajados, type: Integer, default: 0
+  field :monto_horas_extra, type: Float, default: 0.0
+  field :otros_ingresos, type: Float, default: 0.0
+  field :isss_retencion, type: Float, default: 0.0
+  field :afp_retencion, type: Float, default: 0.0
+  field :renta_retencion, type: Float, default: 0.0
+  field :descuento_faltas, type: Float, default: 0.0 # Guarda Faltas + Minutos tarde de gracia
+  field :prestamos_internos, type: Float, default: 0.0
+  field :total_neto, type: Float, default: 0.0
 end
